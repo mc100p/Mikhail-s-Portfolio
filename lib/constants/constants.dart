2aa46c237430a22cbddf931a8d2b145213598c6f
Mikhail Campbell <62410547+mc100p@users.noreply.github.com>
2025-05-07 00:06:50 -0500

    Delete all files

--- lib/constants/constants.dart
+++
@@ -1,6 +0,0 @@
-class StaticText {
-  static const String
-  aboutmeTitle = "About Me",
-  aboutDescription =
-      'Experienced Software Developer, who easily adapts to new technologies and procedures with enthusiasm. A team player, yet also works well on own initiative. Meticulous, with strong knowledge in information technology, computer applications, database management and programming. Possesses a Bachelor of Science in Computer Information Systems from the Northern Caribbean University, Jamaica.';
-}
--- lib/utils/images.dart
+++
@@ -1,4 +0,0 @@
-class ImageNames {
-  static const String profilePicture = 'images/profile_picture.jpeg',
-      adfire = 'images/adfire.png';
-}
